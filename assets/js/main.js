9a3410a9cd2d0326cef287f25a45b295be3e088b
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-12-04 16:04:52 -0300

    Add modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,21 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+const modal = document.getElementById("modal");
+const btn = document.getElementById("open-modal-btn");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
